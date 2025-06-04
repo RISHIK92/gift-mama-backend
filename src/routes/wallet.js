68f9f926b6aad6d13ddcc08d5bd204aaa7280641
Rishik <rishik3555@gmail.com>
2025-06-04 12:54:50 +0530

    final fix

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -8,6 +8,8 @@ import dotenv from "dotenv";
 const router = express.Router();
 const prisma = new PrismaClient();
 
+dotenv.config();
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
